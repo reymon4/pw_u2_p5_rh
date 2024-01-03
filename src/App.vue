1a00f43d39d738efaa212bbe342398a554b8e8e1
Reymon <rahidalgob@uce.edu.ec>
2024-01-03 18:19:56 -0500

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="User:"/>
+  <HelloWorld msg="KING"/>
 </template>
 
 <script>
