a6723b09967a9e104c05a20f1de4c02438e54879
Reymon <rahidalgob@uce.edu.ec>
2024-01-18 23:42:10 -0500

    CorrectionE1

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,157 @@
 <template>
-  <div class="logo">
-    <img
-      alt="Vue logo"
-      src="https://media2.giphy.com/media/Sd9XrDFZZ0Q0OXAdJM/giphy.gif?cid=ecf05e47w43qzv6sua9kx1o3qlxn7quq7u580feiymox3khk&ep=v1_stickers_search&rid=giphy.gif&ct=s"
-    />
-    <!-- <Counter title="Counter1" :value="5" />--->
-    <!-- Escribo la directiva v:bind, cuando requiera que procese código js y en los valores númericos -->
-    <!-- <Counter title="Counter2" :value="calcularValor()" />-->
+  <div class="container" v-if="showGame">
+    <h1>Casino Pokemon</h1>
+    <h2 class="puntaje">Puntaje:{{ puntaje }}</h2>
+    <h2 class="intento">Intento:{{ intento }}</h2>
+    <CorrectionE1 :texto="texto1" :urlImg="url1" />
+    <CorrectionE1 :texto="texto2" :urlImg="url2" />
+    <CorrectionE1 :texto="texto3" :urlImg="url3" />
+
+    <div class="game">
+      <button @click="play()">Play</button>
+    </div>
   </div>
-  <PokemonPage />
+  <div v-if="showLoser">
+    <h1>Has finalizado tus cinco intentos</h1>
+    <h1>Game over. Try again!</h1>
+    <button @click="restart">Restart Game</button>
+  </div>
+
+  <div v-if="showWinner">
+    <h1>Puntaje: {{ puntaje }}</h1>
+    <h1>Felicitaciones has ganado un premio de $10000</h1>
+    <button @click="restart">Restart Game</button>
+  </div>
 </template>
 
 <script>
 /* Cuando importemos siempre ponemos el nombre del archivo. Es un estándar */
 
-import PokemonPage from "./pages/PokemonPage.vue";
-import Question from "./components/Question.vue";
+/* import PokemonPage from "./pages/PokemonPage.vue";
+import Question from "./components/Question.vue"; */
+import CorrectionE1 from "./components/CorrectionE1.vue";
 export default {
   components: {
-    PokemonPage,
+    CorrectionE1,
   },
-  /*  methods: {
-    calcularValor() {
-      return 5 + 5 * 180;
+  data() {
+    return {
+      puntaje: 0,
+      intento: 0,
+      url1: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+      url2: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg",
+      url3: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg",
+      texto1: "xxxxxxxxx",
+      texto2: "xxxxxxxxx",
+      texto3: "xxxxxxxxx",
+      showWinner: false,
+      showLoser: false,
+      showGame: true,
+    };
+  },
+  methods: {
+    async play() {
+      const data1 = await this.consumirAPI();
+      this.texto1 = data1.answer;
+      this.url1 = data1.image;
+
+      const data2 = await this.consumirAPI();
+      this.texto2 = data2.answer;
+      this.url2 = data2.image;
+
+      const data3 = await this.consumirAPI();
+      this.texto3 = data3.answer;
+      this.url3 = data3.image;
+      this.reviewResult();
     },
-  }, */
+    async consumirAPI() {
+      return await fetch("https://yesno.wtf/api").then((r) => r.json());
+    },
+    reviewResult() {
+      this.intento++;
+      if (
+        this.texto1 === "yes" &&
+        this.texto2 === "yes" &&
+        this.texto3 === "yes"
+      ) {
+        this.puntaje += 5;
+      } else if (
+        (this.texto1 === "yes" && this.texto2 === "yes") ||
+        (this.texto1 === "yes" && this.texto3 === "yes") ||
+        (this.texto2 === "yes" && this.texto3 === "yes")
+      ) {
+        this.puntaje = this.puntaje + 2;
+      } else if (
+        this.texto1 === "yes" ||
+        this.texto2 === "yes" ||
+        this.texto3 === "yes"
+      ) {
+        this.puntaje = this.puntaje + 1;
+      }
+      if (this.puntaje >= 10) {
+        this.showWinner = true;
+        this.showGame = false;
+        this.showLoser = false;
+      }
+      if (this.intento === 5) {
+        this.showLoser = true;
+        this.showGame = false;
+        this.showWinner = false;
+      }
+    },
+    restart() {
+      this.puntaje = 0;
+      this.intento = 0;
+      this.url1 =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg";
+      this.url2 =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg";
+      this.url3 =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg";
+      this.texto1 = "xxxxxxxxx";
+      this.texto2 = "xxxxxxxxx";
+      this.texto3 = "xxxxxxxxx";
+      this.showWinner = false;
+      this.showLoser = false;
+      this.showGame = true;
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
 #app {
+  text-align: center;
+  align-items: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
-.logo {
-  height: 200px;
-  align-items: center;
- 
+.container {
+  display: grid;
+  align-content: center;
+  justify-content: center;
+  grid-template-columns: repeat(3, 1fr);
 }
+h1 {
+  grid-column: span 3;
+  text-align: center;
+}
+
+.puntaje {
+  grid-column: 2;
+}
+.intento {
+  grid-column: span 1;
+}
+.game {
+    grid-column: span 3;
+    margin: 0 auto; 
+    display: flex; 
+    align-items: center; 
+  }
+
 
 </style>
---
+++ src/components/CorrectionE1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="conta">
+    <img :src="urlImg" alt="Cannot access to resource">
+    <label for="">{{ texto }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        texto:{
+            type:String,
+        },
+        urlImg:{
+            type:String
+    }
+}
+
+}
+</script>
+
+<style>
+img{
+    width: 200px;
+    height: 200px;
+}
+.conta{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    
+}
+</style>
